694a57a278e06caf37a544663673fcaf6f7705f6
apentyugov <pentyugov@icloud.com>
2022-07-27 17:54:26 +0400

    Refactor position controller endpoints

--- src/main/java/com/pentyugov/wflow/web/rest/PositionController.java
+++ src/main/java/com/pentyugov/wflow/web/rest/PositionController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/position")
+@RequestMapping("/api/positions")
 public class PositionController extends AbstractController {
 
     private final PositionService positionService;
@@ -23,28 +23,28 @@ public class PositionController extends AbstractController {
         this.positionService = positionService;
     }
 
-    @GetMapping("/get-all-positions")
-    public ResponseEntity<Object> getAllPositions() {
+    @GetMapping
+    public ResponseEntity<Object> getAll() {
         List<PositionDto> positions = new ArrayList<>();
         positionService.getAllPositions().forEach(position ->
                 positions.add(positionService.createPositionDtoFromPosition(position)));
         return new ResponseEntity<>(positions, HttpStatus.OK);
     }
 
-    @PostMapping("/add-new-position")
-    public ResponseEntity<PositionDto> addNewPosition(@RequestBody PositionDto positionDto) throws PositionExistException {
+    @PostMapping
+    public ResponseEntity<PositionDto> add(@RequestBody PositionDto positionDto) throws PositionExistException {
         Position position = positionService.createNewPosition(positionDto);
         return new ResponseEntity<>(positionService.createPositionDtoFromPosition(position), HttpStatus.OK);
     }
 
-    @PutMapping("/update-position")
-    public ResponseEntity<PositionDto> updatePosition(@RequestBody PositionDto positionDto) throws PositionExistException {
+    @PutMapping
+    public ResponseEntity<PositionDto> update(@RequestBody PositionDto positionDto) throws PositionExistException {
         Position position = positionService.updatePosition(positionDto);
         return new ResponseEntity<>(positionService.createPositionDtoFromPosition(position), HttpStatus.OK);
     }
 
-    @DeleteMapping("/delete-position/{id}")
-    public ResponseEntity<HttpResponse> deletePosition(@PathVariable String id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity<HttpResponse> delete(@PathVariable String id) {
         positionService.deletePosition(UUID.fromString(id));
         String message = String.format("Position with id: %s was deleted", id);
         return response(HttpStatus.OK, message);
